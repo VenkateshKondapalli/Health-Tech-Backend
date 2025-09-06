d3d00394764533a9859b1c5a84174fa282087069
venkatesh <venkateshkondapall18k@gmail.com>
2025-09-06 18:26:36 +0530

    added generic error handler

--- api/patients/patient.controller.js
+++ api/patients/patient.controller.js
@@ -1,74 +1,95 @@
 const { patientModel } = require("../../model/patient.schema");
+const { HandleGenericAPIError } = require("../../utils/controller.helper");
 
 const addPatientController = async (req, res) => {
-  const { _id: createdBy } = req.user;
-  const exist = await patientModel.findOne({ email: req.body.email });
-  if (exist) {
-    return res.status(400).json({
-      isSuccess: false,
-      message: "patient already exist",
-      data: {},
+  try {
+    const { _id: createdBy } = req.user;
+    const exist = await patientModel.findOne({ email: req.body.email });
+    if (exist) {
+      return res.status(400).json({
+        isSuccess: false,
+        message: "patient already exist",
+        data: {},
+      });
+    }
+
+    const patient = await patientModel.create({ ...req.body, createdBy });
+
+    res.status(201).json({
+      isSuccess: true,
+      message: "patient added successfully",
+      data: patient,
     });
+  } catch (err) {
+    HandleGenericAPIError("addPatientController", res, err);
   }
-
-  const patient = await patientModel.create({ ...req.body, createdBy });
-
-  res.status(201).json({
-    isSuccess: true,
-    message: "patient added successfully",
-    data: patient,
-  });
 };
 
 const getAllPatientsData = async (req, res) => {
-  const patientsData = await patientModel
-    .find({ createdBy: req.user._id })
-    .lean();
-  if (!patientsData) {
-    return req.status(400).json({
-      isSuccess: false,
-      message: "no patients created by user",
-      data: {},
-    });
-  }
+  try {
+    const patientsData = await patientModel
+      .find({ createdBy: req.user._id })
+      .lean();
+    if (!patientsData) {
+      return req.status(400).json({
+        isSuccess: false,
+        message: "no patients created by user",
+        data: {},
+      });
+    }
 
-  res.status(200).json({
-    isSuccess: true,
-    message: "patients data fetched successfully",
-    data: patientsData,
-  });
+    res.status(200).json({
+      isSuccess: true,
+      message: "patients data fetched successfully",
+      data: patientsData,
+    });
+  } catch (err) {
+    HandleGenericAPIError("getAllPatientsData", res, err);
+  }
 };
 
 const getPatientById = async (req, res) => {
-  const patient = await patientModel.findById(req.params.id).lean();
+  try {
+    const patient = await patientModel.findById(req.params.id).lean();
 
-  res.status(200).json({
-    isSuccess: true,
-    message: "patient found",
-    data: patient,
-  });
+    res.status(200).json({
+      isSuccess: true,
+      message: "patient found",
+      data: patient,
+    });
+  } catch (err) {
+    HandleGenericAPIError("getPatientById", res, err);
+  }
 };
 
 const updatePatient = async (req, res) => {
-  const updatedpatient = await patientModel.findByIdAndUpdate(
-    req.params.id,
-    req.body
-  );
+  try {
+    const updatedpatient = await patientModel.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    );
 
-  res.status(201).json({
-    isSucess: true,
-    message: "patient data updated sucessfully",
-    data: updatedpatient,
-  });
+    res.status(201).json({
+      isSucess: true,
+      message: "patient data updated sucessfully",
+      data: updatedpatient,
+    });
+  } catch (err) {
+    HandleGenericAPIError("updatePatient", res, err);
+  }
 };
 
 const deletePatient = async (req, res) => {
-  const deletedData = await patientModel.findByIdAndDelete(req.params.id);
-  res.status(204).json({
-    isSuccess: true,
-    message: "patient deleted sucessfully",
-    data: deletedData,
-  });
+  try {
+    const deletedData = await patientModel.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      isSuccess: true,
+      message: "patient deleted sucessfully",
+      data: deletedData,
+    });
+  } catch (err) {
+    HandleGenericAPIError("deletePatient", res, err);
+  }
 };
 module.exports = {
   addPatientController,
--- api/patients/patient.dto.js
+++ api/patients/patient.dto.js
@@ -1,27 +1,36 @@
 const { patientModel } = require("../../model/patient.schema");
+const { HandleGenericAPIError } = require("../../utils/controller.helper");
 
 const addNewPatientValidator = async (req, res, next) => {
-  const { name, gender, dateOfBirth, phone } = req.body;
+  try {
+    const { name, gender, dateOfBirth, phone } = req.body;
 
-  if (!name || !gender || !dateOfBirth || !phone) {
-    return res.status(400).json({
-      isSucess: false,
-      message: "name ,gender, date of birth and phone are required",
-      data: {},
-    });
+    if (!name || !gender || !dateOfBirth || !phone) {
+      return res.status(400).json({
+        isSucess: false,
+        message: "name ,gender, date of birth and phone are required",
+        data: {},
+      });
+    }
+    next();
+  } catch (err) {
+    HandleGenericAPIError("addNewPatientValidator", res, err);
   }
-  next();
 };
 
 const isPatientexist = async (req, res, next) => {
-  const exist = await patientModel.findById(req.params.id).lean();
-  if (!exist) {
-    return res.status(404).json({
-      isSuccess: false,
-      message: "patient not exist",
-      data: {},
-    });
+  try {
+    const exist = await patientModel.findById(req.params.id).lean();
+    if (!exist) {
+      return res.status(404).json({
+        isSuccess: false,
+        message: "patient not exist",
+        data: {},
+      });
+    }
+    next();
+  } catch (err) {
+    HandleGenericAPIError("isPatientexist", res, err);
   }
-  next();
 };
 module.exports = { addNewPatientValidator, isPatientexist };
